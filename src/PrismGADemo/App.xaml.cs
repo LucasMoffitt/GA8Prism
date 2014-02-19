00734629fa626ac70f0aa2b91a7254429a296f9b
Lucas Moffitt <LucasMoffitt@Gmail.com>
2014-02-19 19:15:22 +1100

    Magical 1's and 0's

---
+++ src/PrismGADemo/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using DT.GoogleAnalytics.Metro;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Unity;
+
+namespace PrismGADemo
+{
+    sealed partial class App : MvvmAppBase
+    {
+        private readonly UnityContainer _container = new UnityContainer();
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplication(LaunchActivatedEventArgs args)
+        {
+            AnalyticsHelper.Setup();
+
+            NavigationService.Navigate("Main", null);
+
+            return Task.FromResult<object>(null);
+        }
+
+        protected override void OnInitialize(IActivatedEventArgs args)
+        {
+            _container.RegisterInstance(SessionStateService);
+            _container.RegisterInstance(NavigationService);
+
+            ViewModelLocator.SetDefaultViewModelFactory(viewModelType => _container.Resolve(viewModelType));
+        }
+    }
+}
---
+++ src/PrismGADemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DT.GoogleAnalytics.Metro;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Prism.StoreApps.Interfaces;
+
+namespace PrismGADemo.ViewModels
+{
+    class MainPageViewModel
+    {
+        public DelegateCommand FireEventCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            AnalyticsHelper.TrackPageView("PrismGADemo Main");
+
+            FireEventCommand = new DelegateCommand(() => AnalyticsHelper.Track("PrismDemo", "EventTest"));
+        }
+    }
+}
